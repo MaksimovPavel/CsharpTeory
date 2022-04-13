dcaf38d454ed808b52f176c07e406d35f0e4f0e1
Pavel <89213158645p@gmail.com>
2022-04-13 17:44:07 +0300

    Example005 added

---
+++ Example005_ConditionElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "пахан")
+{
+    Console.WriteLine("Пахан, мое почтение!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
